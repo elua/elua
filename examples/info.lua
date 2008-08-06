f723f9f7954978ced4361cf248f9af0a915bbb19
Bogdan Marinescu <bogdan.marinescu@gmail.com>
2008-08-06 15:26:25 +0000

    eLua examples are now part of the repository

---
+++ examples/info.lua
@@ -0,0 +1,2 @@
+print( "I'm running on platform " .. pd.name() )
+print( "The CPU clock is " .. pd.clock() )
---
+++ examples/led.lua
@@ -0,0 +1,25 @@
+-- eLua test 
+
+if pd.name() == "AT91SAM7X" then
+  ledpin = pio.PB_20
+elseif pd.name() == "LM3S" then
+  ledpin = pio.PF_0
+else
+  ledpin = pio.P9_0
+end
+
+function cycle()
+  pio.set( ledpin )
+  tmr.delay( tmr.TMR0, 500000 )
+  pio.clear( ledpin )
+  tmr.delay( tmr.TMR0, 500000 )
+end
+
+pio.output( ledpin )
+print( "Hello from eLua on " .. pd.name() )
+print "Watch your LED blinking :)"
+print "Press any key to end this demo.\n"
+
+while uart.recv( uart.UART0, 0, 0 ) < 0 do
+  cycle()
+end
