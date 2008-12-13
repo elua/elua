77d626b26a034317dce0e6522440d2218c5def2e
Dado Sutter <dadosutter@gmail.com>
2008-12-13 21:31:20 +0000

    href tags changed to relative (to current host) to avoid the need for "elua" host be known by DNS. Modules tested ok after changes.

--- romfs/test.lua
+++ romfs/test.lua
@@ -1,4 +1,4 @@
 print "<html><body>"
 print "Hello from eLua!<br>"
-print 'Press <a href="http://elua/">here</a> to return to the main page.'
+print 'Press <a href="/">here</a> to return to the main page.'
 print "</body></html>"
