7da5ed993043528c696b16645f5ccafba263e076
James Snyder <jbsnyder@fanplastic.org>
2009-06-29 00:27:45 +0000

    Touchups for documentation & comments for adc* scripts from romfs.

--- romfs/adcpoll.lua
+++ romfs/adcpoll.lua
@@ -1,3 +1,4 @@
+-- Acquire ADC samples using a timer with polling for available samples
 
 if pd.board() == "ET-STM32" then
   timer = 2
@@ -9,16 +10,17 @@ else
   adcsmoothing = {4, 64, 32, 16}
 end
 
+-- Setup ADC and start sampling
 for i, v in ipairs(adcchannels) do
-  adc.setblocking(v,0)
-  adc.setsmoothing(v,adcsmoothing[i])
-  adc.setclock(v, 4 ,timer)
+  adc.setblocking(v,0) -- no blocking on any channels
+  adc.setsmoothing(v,adcsmoothing[i]) -- set smoothing from adcsmoothing table
+  adc.setclock(v, 4 ,timer) -- get 4 samples per second, per channel
 end
 
-adc.sample(adcchannels,128)
+adc.sample(adcchannels,128) -- start sampling on all channels
 
+-- Draw static text on terminal
 term.clrscr()
-
 term.gotoxy(1,1)
 term.putstr("ADC Status:")
 term.gotoxy(1,3)
@@ -26,23 +28,23 @@ term.putstr(" CH   SLEN   RES")
 term.gotoxy(1,#adcchannels+5)
 term.putstr("Press ESC to exit.")
 
+-- use some locals for speed
 local sample = adc.sample
 local getsample = adc.getsample
 local samplesready = adc.samplesready
-local i, v
-local tsample
+local i, v, tsample
 
 while true do
   for i, v in ipairs(adcchannels) do
-    tsample = getsample(v)
-    if not (tsample == nil) then
+    tsample = getsample(v) -- try to get a sample
+    if not (tsample == nil) then -- if we have a new sample, then update display
     	term.gotoxy(1,i+3)
     	term.putstr(string.format("ADC%02d (%03d): %04d\n", v, adcsmoothing[i], tsample))
     end
-    if adc.isdone(v) == 1 then adc.sample(v,128) end
+    if adc.isdone(v) == 1 then adc.sample(v,128) end -- if samples have run out, start collection again
   end
   key = term.getch( term.NOWAIT )
-  if key == term.KC_ESC then break end
+  if key == term.KC_ESC then break end -- exit if user hits Escape
 end
 
 term.clrscr()
