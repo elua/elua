841ba4006ec6801bdf1bc9e5f37971df8e766fed
James Snyder <jbsnyder@fanplastic.org>
2009-06-29 16:42:24 +0000

    new, better doc generation tool, now support semi-automatic generation of links to function names/structures in the navtree, also added support for multiple languages

--- doc/arch_platform/arch_platform_eth.lua
+++ doc/arch_platform/arch_platform_eth.lua
@@ -1,92 +1,98 @@
 -- eLua platform interface - Ethernet support
 
--- Title
-title = "eLua platform interface - Ethernet support"
-
--- Overview
-overview = [[<font color="red">$NOTE$: TCP/IP support is experimental in eLua. Although functional, it's quite incomplete at the moment.</font></p>
-<p>This part of the platform interface groups functions related to accessing the Ethernet interface (internal or external) of the CPU. Note that unlike the
-other parts of the platform interface this one is dedicated for TCP/IP support and thus it does not correspond directly to an eLua module, although
-the @##@net module@ is implemented with functions that rely on this part of the platform interface. Currently only
-the ^http://www.sics.se/~~adam/uip/index.php/Main_Page^uIP^ TCP/IP stack is supported by eLua.</p>
-<p>uIP is implemented in eLua using two hardware interrupts (that should be available on your platform): the Ethernet receive interrupt (to handle
-incoming packets) and a timer interrupt (timers are used internally by uIP). However, the uIP main loop is only called from the Ethernet interrupt handler
-in eLua, so in order to acknowledge the timer interrupt (as well as to provide some optimizations) a function that "forces" an Ethernet interrupt
-must also be provided by the platform interface (see @#force@here@ for details).</p>
-<p>To put everything together, part of the Ethernet platform interface for the $m3s$ platform is given below:</p>
-~u32 platform_eth_get_elapsed_time()
+data_en = 
 {
-  if( eth_timer_fired )
+  -- Title
+  title = "eLua platform interface - Ethernet support",
+
+  -- Menu name
+  menu_name = "Ethernet",
+
+  -- Overview
+  overview = [[<font color="red">$NOTE$: TCP/IP support is experimental in eLua. Although functional, it's quite incomplete at the moment.</font></p>
+  <p>This part of the platform interface groups functions related to accessing the Ethernet interface (internal or external) of the CPU. Note that unlike the
+  other parts of the platform interface this one is dedicated for TCP/IP support and thus it does not correspond directly to an eLua module, although
+  the @##@net module@ is implemented with functions that rely on this part of the platform interface. Currently only
+  the ^http://www.sics.se/~~adam/uip/index.php/Main_Page^uIP^ TCP/IP stack is supported by eLua.</p>
+  <p>uIP is implemented in eLua using two hardware interrupts (that should be available on your platform): the Ethernet receive interrupt (to handle
+  incoming packets) and a timer interrupt (timers are used internally by uIP). However, the uIP main loop is only called from the Ethernet interrupt handler
+  in eLua, so in order to acknowledge the timer interrupt (as well as to provide some optimizations) a function that "forces" an Ethernet interrupt
+  must also be provided by the platform interface (see @#platform_eth_force_interrupt@here@ for details).</p>
+  <p>To put everything together, part of the Ethernet platform interface for the $m3s$ platform is given below:</p>
+  ~u32 platform_eth_get_elapsed_time()
   {
-    eth_timer_fired = 0;
-    return SYSTICKMS;
-  }
-  else
-    return 0;
-}
-
-void SysTickIntHandler()
-{
-  // Handle virtual timers
-  cmn_virtual_timer_cb();
-
-  // Indicate that a SysTick interrupt has occurred.
-  eth_timer_fired = 1;
-
-  // Generate a fake Ethernet interrupt.  This will perform the actual work
-  // of incrementing the timers and taking the appropriate actions.
-  <b>platform_eth_force_interrupt();</b>
-}
-
-void EthernetIntHandler()
-{
-  u32 temp;
-
-  // Read and Clear the interrupt.
-  temp = EthernetIntStatus( ETH_BASE, false );
-  EthernetIntClear( ETH_BASE, temp );
-
-  // Call the UIP main loop
-  <b>elua_uip_mainloop();</b>
-}~<p>]]
-
--- Functions
-funcs = 
-{
-  { sig = "void platform_eth_send_packet( const void* src, u32 size )",
-    desc = "Sends an Ethernet packet to the network",
-    args = 
+    if( eth_timer_fired )
     {
-      { name = "src", desc = "start address of the Ethernet packet" },
-      { name = "size", desc = "size of the Ethernet packet" }
-    },
-  },
-
-  { sig = "u32 platform_eth_get_packet_nb( void* buf, u32 maxlen );",
-    desc = "Non-blocking read of an Ethernet packet from the network",
-    args = 
-    {
-      { name = "buf", desc = "start address of the receive buffer" },
-      { name = "maxlen", desc = "maximum length of the Ethernet packet" },
-    },
-    ret = 
-    {
-      "the size of the read packet or 0 if no packet is available"
+      eth_timer_fired = 0;
+      return SYSTICKMS;
     }
-  },
+    else
+      return 0;
+  }
 
-  { sig = "void platform_eth_force_interrupt();",
-    link = "force",
-    desc = "Force the Ethernet interrupt on the platform (see @#overview@overview@ above for details)",
-  },
+  void SysTickIntHandler()
+  {
+    // Handle virtual timers
+    cmn_virtual_timer_cb();
 
-  { sig = "u32 platform_eth_get_elapsed_time();",
-    desc = [[Get the elapsed time (in ms) since the last invocation of the uIP main loop ($elua_uip_mainloop$, from which this function is called). See @#overview@overview@ for a possible 
-implementation of this function).]], 
-    ret = 
-    { 
-      "0 if the uIP loop was called because of Ethernet activity, not because a timer expired",
-      "the Ethernet timer perios in ms (which indicates timer activity)"
+    // Indicate that a SysTick interrupt has occurred.
+    eth_timer_fired = 1;
+
+    // Generate a fake Ethernet interrupt.  This will perform the actual work
+    // of incrementing the timers and taking the appropriate actions.
+    <b>platform_eth_force_interrupt();</b>
+  }
+
+  void EthernetIntHandler()
+  {
+    u32 temp;
+
+    // Read and Clear the interrupt.
+    temp = EthernetIntStatus( ETH_BASE, false );
+    EthernetIntClear( ETH_BASE, temp );
+
+    // Call the UIP main loop
+    <b>elua_uip_mainloop();</b>
+  }~<p>]],
+
+  -- Functions
+  funcs = 
+  {
+    { sig = "void #platform_eth_send_packet#( const void* src, u32 size )",
+      desc = "Sends an Ethernet packet to the network",
+      args = 
+      {
+        { name = "src", desc = "start address of the Ethernet packet" },
+        { name = "size", desc = "size of the Ethernet packet" }
+      },
     },
+
+    { sig = "u32 #platform_eth_get_packet_nb#( void* buf, u32 maxlen );",
+      desc = "Non-blocking read of an Ethernet packet from the network",
+      args = 
+      {
+        { name = "buf", desc = "start address of the receive buffer" },
+        { name = "maxlen", desc = "maximum length of the Ethernet packet" },
+      },
+      ret = 
+      {
+        "the size of the read packet or 0 if no packet is available"
+      }
+    },
+
+    { sig = "void #platform_eth_force_interrupt#();",
+      desc = "Force the Ethernet interrupt on the platform (see @#overview@overview@ above for details)",
+    },
+
+    { sig = "u32 #platform_eth_get_elapsed_time#();",
+      desc = [[Get the elapsed time (in ms) since the last invocation of the uIP main loop ($elua_uip_mainloop$, from which this function is called). See @#overview@overview@ for a possible 
+  implementation of this function).]], 
+      ret = 
+      { 
+        "0 if the uIP loop was called because of Ethernet activity, not because a timer expired",
+        "the Ethernet timer perios in ms (which indicates timer activity)"
+      },
+    }
   }
 }
+
--- doc/arch_platform/arch_platform_uart.lua
+++ doc/arch_platform/arch_platform_uart.lua
@@ -1,122 +1,128 @@
 -- eLua platform interface - UART
 
--- Title
-title = "eLua platform interface - UART"
-
--- Overview
-overview = "This part of the platform interface groups functions related to the UART interface(s) of the MCU."
-
--- Data structures, constants and types
-structures = 
+data_en = 
 {
-  { text = [[// Parity
+  -- Title
+  title = "eLua platform interface - UART",
+
+  -- Menu name
+  menu_name = "UART",
+
+  -- Overview
+  overview = "This part of the platform interface groups functions related to the UART interface(s) of the MCU.",
+
+  -- Data structures, constants and types
+  structures = 
+  {
+    { text = [[// Parity
 enum
 {
   PLATFORM_UART_PARITY_EVEN,
   PLATFORM_UART_PARITY_ODD,
   PLATFORM_UART_PARITY_NONE
 };]],
-    link = "parval",
-    desc = "Constants used to specify the UART parity mode."
-  },
+      name = "UART parity",
+      desc = "Constants used to specify the UART parity mode."
+    },
 
-  { text = [[// Stop bits
+    { text = [[// Stop bits
 enum
 {
   PLATFORM_UART_STOPBITS_1,
   PLATFORM_UART_STOPBITS_1_5,
   PLATFORM_UART_STOPBITS_2
 };]],
-    link = "stopval",
-    desc = "Constants used to specify the number of UART stop bits.",
-  },
+      name = "UART stop bits",
+      desc = "Constants used to specify the number of UART stop bits.",
+    },
 
-  { text = [[// "Infinite timeout" constant for recv
+    { text = [[// "Infinite timeout" constant for recv
 #define PLATFORM_UART_INFINITE_TIMEOUT        (-1)]],
-    link = "toinf",
-    desc = "This constant is used as a special timeout value (infinite timeout) in the UART functions that expect a timeout as argument.",
-  }
-}
-
--- Functions
-funcs = 
-{
-  { sig = "int platform_uart_exists( unsigned id );",
-    desc = [[Checks if the platform has the hardware UART specified as argument. Implemented in %src/common.c%, it uses the $NUM_UART$ macro that must be defined in the
-platform's $platform_conf.h$ file (see @arch_overview.html#platforms@here@ for details). For example:</p>
-~#define NUM_UART   2      $// The platform has 2 UART interfaces$~<p>]],
-    args = 
-    {
-      { name = "id", desc = "UART interface ID" },
-    },
-    ret = "1 if the specified UART exists, 0 otherwise"
-  },
-
-  { sig = "u32 platform_uart_setup( unsigned id, u32 baud, int databits, int parity, int stopbits );",
-    desc = "This function is used to initialize the parameters of the UART interface.",
-    args = 
-    {
-      { name = "id", desc = "UART interface ID." },
-      { name = "baud", desc = "baud rate." },
-      { name = "databits", desc = "number of databits (maximum 8)." },
-      { name = "parity", desc = "parity type (can be either $PLATFORM_UART_PARITY_EVEN$, $PLATFORM_UART_PARITY_ODD$ or $PLATFORM_UART_PARITY_NONE$, see @#parval@here@)." },
-      { name = "stopbits", desc = [[number of stop bits (can be either $PLATFORM_UART_STOPBITS_1$, $PLATFORM_UART_STOPBITS_1_5$ or $PLATFORM_UART_STOPBITS_2$, see
-         @#stopval@here@).]] },
-    },
-    ret = "the actual baud rate. Depending on the hardware, this may have a different value than the $baud$ argument.",
-  },
-
-  { sig = "void platform_uart_send( unsigned id, u8 data );",
-    desc = "Send data to an UART interface.",
-    args = 
-    {
-      { name = "id", desc = "UART interface ID." },
-      { name = "data", desc = "data to be sent." },
-    },
-  },
-
-  { sig = "int platform_uart_recv( unsigned id, unsigned timer_id, s32 timeout );",
-    link = "platform_uart_recv",
-    desc = [[Receive data from the UART interface (blocking/non blocking with timeout/immediate).<br>
-This function is "split" in two parts: a platform-independent part that is implemented in %src/common.c%, and a platform-dependent part that must be implemented by each
-platform in a function named @#platform_s_uart_recv@platform_s_uart_recv@.]],
-    args = 
-    {
-      { name = "id", desc = "UART interface ID." },
-      { name = "timer_id", desc = "the ID of the timer used in this operation (see @arch_platform_timers.html@here@ for details). See also the description of the $timeout$ argument." },
-      { name = "timeout", desc = [[specifies a timeout for the receive operation as follows:
-<ul>
-  <li>$timeout &gt; 0$: the timer with the specified $timer_id$ will be used to timeout the receive operation after $timeout$ microseconds.</li>
-  <li>$timeout = 0$: the function returns immediately regardless of data being available or not. $timer_id$ is ignored.</li>
-  <li>$timeout$ = @#toinf@PLATFORM_UART_INFINITE_TIMEOUT@: the function waits indefinitely for UART data to be available and returns it. In this mode the function doesn't 
-      time out, so $timer_id$ is ignored.</li>
-</ul>]] },
-    },
-    ret = 
-    {
-      "if $timeout &gt; 0$ and data from the UART is available in $timeout$ microseconds of less it is returned, otherwise -1 is returned",
-      "if $timeout = 0$ and data from the UART is available when the function is called it is returned, otherwise -1 is returned",
-      "if $timeout$ = @#toinf@PLATFORM_UART_INIFINITE_TIMEOUT@ it returns the data read from the UART after it becomes available"
+      name = "UART timeout",
+      desc = "This constant is used as a special timeout value (infinite timeout) in the UART functions that expect a timeout as argument.",
     }
   },
 
-  { sig = "int platform_s_uart_recv( unsigned id, s32 timeout );",
-    link = "platform_s_uart_recv",
-    desc = [[This is the platform-dependent part of the UART receive function @#platform_uart_recv@platform_uart_recv@, and is in fact a "subset" of the full function 
-(thus being easier to implement by each platform in part). In particular, it never needs to deal with the $timeout &gt; 0$ case, which is handled by @#platform_uart_recv@platform_uart_recv@.]],
-     args = 
-    {
-      { name = "id", desc = "UART interface ID." },
-      { name = "timeout", desc = [[specifies a timeout for the receive operation as follows:
-<ul>
-  <li>$timeout = 0$: the function returns immediately regardless of data being available or not.</li>
-  <li>$timeout$ = @#toinf@PLATFORM_UART_INFINITE_TIMEOUT@: the function waits indefinitely for UART data to be available and returns it.</li>
-</ul>]] },
+  -- Functions
+  funcs = 
+  {
+    { sig = "int #platform_uart_exists#( unsigned id );",
+      desc = [[Checks if the platform has the hardware UART specified as argument. Implemented in %src/common.c%, it uses the $NUM_UART$ macro that must be defined in the
+  platform's $platform_conf.h$ file (see @arch_overview.html#platforms@here@ for details). For example:</p>
+  ~#define NUM_UART   2      $// The platform has 2 UART interfaces$~<p>]],
+      args = 
+      {
+        { name = "id", desc = "UART interface ID" },
+      },
+      ret = "1 if the specified UART exists, 0 otherwise"
     },
-    ret = 
-    {
-      "if $timeout = 0$ and data from the UART is available when the function is called it is returned, otherwise -1 is returned",
-      "if $timeout$ = @#toinf@PLATFORM_UART_INIFINITE_TIMEOUT@ it returns the data read from the UART after it becomes available"
+
+    { sig = "u32 #platform_uart_setup#( unsigned id, u32 baud, int databits, int parity, int stopbits );",
+      desc = "This function is used to initialize the parameters of the UART interface.",
+      args = 
+      {
+        { name = "id", desc = "UART interface ID." },
+        { name = "baud", desc = "baud rate." },
+        { name = "databits", desc = "number of databits (maximum 8)." },
+        { name = "parity", desc = "parity type (can be either $PLATFORM_UART_PARITY_EVEN$, $PLATFORM_UART_PARITY_ODD$ or $PLATFORM_UART_PARITY_NONE$, see @#uart_parity@here@)." },
+        { name = "stopbits", desc = [[number of stop bits (can be either $PLATFORM_UART_STOPBITS_1$, $PLATFORM_UART_STOPBITS_1_5$ or $PLATFORM_UART_STOPBITS_2$, see
+           @#uart_stop_bits@here@).]] },
+      },
+      ret = "the actual baud rate. Depending on the hardware, this may have a different value than the $baud$ argument.",
+    },
+
+    { sig = "void #platform_uart_send#( unsigned id, u8 data );",
+      desc = "Send data to an UART interface.",
+      args = 
+      {
+        { name = "id", desc = "UART interface ID." },
+        { name = "data", desc = "data to be sent." },
+      },
+    },
+
+    { sig = "int #platform_uart_recv#( unsigned id, unsigned timer_id, s32 timeout );",
+      link = "platform_uart_recv",
+      desc = [[Receive data from the UART interface (blocking/non blocking with timeout/immediate).<br>
+  This function is "split" in two parts: a platform-independent part that is implemented in %src/common.c%, and a platform-dependent part that must be implemented by each
+  platform in a function named @#platform_s_uart_recv@platform_s_uart_recv@.]],
+      args = 
+      {
+        { name = "id", desc = "UART interface ID." },
+        { name = "timer_id", desc = "the ID of the timer used in this operation (see @arch_platform_timers.html@here@ for details). See also the description of the $timeout$ argument." },
+        { name = "timeout", desc = [[specifies a timeout for the receive operation as follows:
+  <ul>
+    <li>$timeout &gt; 0$: the timer with the specified $timer_id$ will be used to timeout the receive operation after $timeout$ microseconds.</li>
+    <li>$timeout = 0$: the function returns immediately regardless of data being available or not. $timer_id$ is ignored.</li>
+    <li>$timeout$ = @#uart_timeout@PLATFORM_UART_INFINITE_TIMEOUT@: the function waits indefinitely for UART data to be available and returns it. In this mode the function doesn't 
+        time out, so $timer_id$ is ignored.</li>
+  </ul>]] },
+      },
+      ret = 
+      {
+        "if $timeout &gt; 0$ and data from the UART is available in $timeout$ microseconds of less it is returned, otherwise -1 is returned",
+        "if $timeout = 0$ and data from the UART is available when the function is called it is returned, otherwise -1 is returned",
+        "if $timeout$ = @#uart_timeout@PLATFORM_UART_INIFINITE_TIMEOUT@ it returns the data read from the UART after it becomes available"
+      }
+    },
+
+    { sig = "int #platform_s_uart_recv#( unsigned id, s32 timeout );",
+      link = "platform_s_uart_recv",
+      desc = [[This is the platform-dependent part of the UART receive function @#platform_uart_recv@platform_uart_recv@, and is in fact a "subset" of the full function 
+  (thus being easier to implement by each platform in part). In particular, it never needs to deal with the $timeout &gt; 0$ case, which is handled by @#platform_uart_recv@platform_uart_recv@.]],
+       args = 
+      {
+        { name = "id", desc = "UART interface ID." },
+        { name = "timeout", desc = [[specifies a timeout for the receive operation as follows:
+  <ul>
+    <li>$timeout = 0$: the function returns immediately regardless of data being available or not.</li>
+    <li>$timeout$ = @#uart_timeout@PLATFORM_UART_INFINITE_TIMEOUT@: the function waits indefinitely for UART data to be available and returns it.</li>
+  </ul>]] },
+      },
+      ret = 
+      {
+        "if $timeout = 0$ and data from the UART is available when the function is called it is returned, otherwise -1 is returned",
+        "if $timeout$ = @#uart_timeout@PLATFORM_UART_INIFINITE_TIMEOUT@ it returns the data read from the UART after it becomes available"
+      }
     }
   }
 }
