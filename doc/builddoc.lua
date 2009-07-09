49d40852298ff7ee0f5900466b4ce5d65eb63e9d
James Snyder <jbsnyder@fanplastic.org>
2009-07-09 03:15:51 +0000

    Basic tmr documentation.

--- doc/builddoc.lua
+++ doc/builddoc.lua
@@ -7,7 +7,7 @@ local doc_sections = { "arch_platform", "refman_gen" }
 local components = 
 { 
   arch_platform = { "ll", "pio", "spi", "uart", "timers", "pwm", "cpu", "eth", "adc" },
-  refman_gen = { "bit", "pd", "cpu", "pack", "pwm", "adc" }
+  refman_gen = { "bit", "pd", "pack", "tmr", "pwm", "cpu", "adc" }
 }
 
 -- List here all languages for the documentation (make sure to keep English ("en") the first one)
