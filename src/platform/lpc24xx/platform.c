6008d472a1f2d3ee4c8071d5fda059f335b9e55e
Bogdan Marinescu <bogdan.marinescu@gmail.com>
2009-10-26 21:46:04 +0000

    Added basic platform support files for the lpc24xx port. The port compiles, but doesn't do anything yet. Oh well, at least it compiles :)

---
+++ src/platform/lpc24xx/platform.c
@@ -0,0 +1,26 @@
+// Platform-dependent functions
+
+#include "platform.h"
+#include "type.h"
+#include "devman.h"
+#include "genstd.h"
+#include "stacks.h"
+#include <reent.h>
+#include <errno.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include "utils.h"
+#include "common.h"
+#include "platform_conf.h"
+
+// Platform includes
+
+// ****************************************************************************
+// Platform initialization
+
+int platform_init()
+{
+  return PLATFORM_OK;
+} 
+
