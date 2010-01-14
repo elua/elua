49d8db9312d461b5efb6e9fb7e9bdcc8b81bc748
Bogdan Marinescu <bogdan.marinescu@gmail.com>
2010-01-14 20:52:26 +0000

    updated OpenOCD with info about the STR-E912 board

--- doc/docdata.lua
+++ doc/docdata.lua
@@ -75,6 +75,7 @@ local menu =
       },
       { { "Old versions", "Versões Anteriores" }, "dl_old.html", 
         {
+          { "0.6", "dl_old.html#v060" },
           { "0.5", "dl_old.html#v050" },
           { "0.4.1", "dl_old.html#v041" },
           { "0.4", "dl_old.html#v04" },
