82a940e9d16e4e1d60aacf38f9a7dbf8576a80e2
James Snyder <jbsnyder@fanplastic.org>
2010-01-15 01:19:05 +0000

    Disable readline on windows.

--- rpc-lua.py
+++ rpc-lua.py
@@ -10,18 +10,21 @@ lua_files = """lapi.c lcode.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c
 lua_full_files = " " + " ".join( [ "src/lua/%s" % name for name in lua_files.split() ] )
 lua_full_files += " src/modules/luarpc.c src/luarpc_posix_serial.c "
 
+linkcom = "gcc -o $TARGET $SOURCES -lm"
+
 if platform.system() == "Windows":
   lua_full_files += " src/serial/serial_win32.c"
   cdefs += " -DWIN32_BUILD "
 else:
   lua_full_files += " src/serial/serial_posix.c"
+  linkcom += " -lreadline"
   cdefs += " -DLUA_USE_READLINE "
 
 local_include = "-Isrc/lua -Iinc -Isrc/modules -Iinc/serial"
 
 # Compiler/linker options
 cccom = "gcc -g %s -Wall %s -c $SOURCE -o $TARGET" % ( local_include, cdefs )
-linkcom = "gcc -o $TARGET $SOURCES -lm -lreadline"
+
 
 # Env for building the program
 comp = Environment( CCCOM = cccom,
