47f9d970796b64ec5c442a9186d6f1cb52eb9543
James Snyder <jbsnyder@fanplastic.org>
2010-01-27 00:56:17 +0000

    Missing platform_conf.h.

---
+++ inc/desktop/platform_conf.h
@@ -0,0 +1,17 @@
+// eLua platform configuration
+
+#ifndef __PLATFORM_CONF_H__
+#define __PLATFORM_CONF_H__
+
+#ifdef LUA_RPC
+#include "auxmods.h"
+
+#define BUILD_RPC
+#define LUARPC_ENABLE_SERIAL
+
+#define LUA_PLATFORM_LIBS_ROM \
+  _ROM( AUXLIB_RPC, luaopen_rpc, rpc_map )
+#endif
+
+
+#endif // #ifndef __PLATFORM_CONF_H__
