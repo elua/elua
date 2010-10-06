b7c8d7dddf5007c6123cba9cd425db1a83eb0897
Bogdan Marinescu <bogdan.marinescu@gmail.com>
2010-10-06 18:05:09 +0000

    added a reference to the #elua IRC channel in the community page

--- doc/docdata.lua
+++ doc/docdata.lua
@@ -44,6 +44,7 @@ local menu =
     {
       { { "Discussion Lists", "Listas de Discussão" }, "comunity.html#lists" },
       { "Forums", "comunity.html#forums" },
+      { "IRC", "comunity.html#irc" },
       { "User Labs Wiki", "comunity.html#userswiki" },      
       { { "Credits", "Créditos" }, "comunity.html#credits" },
       { nil, "forum.html", nil, "Forum" } -- "hidden" entry (doesn't appear in the menu)
