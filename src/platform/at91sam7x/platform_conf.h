fd8755e50880e3e9ca5069685a239d08960818cc
Bogdan Marinescu <bogdan.marinescu@gmail.com>
2010-11-20 23:34:29 +0000

    RFS + sermux + proper BUF_ENABLE_ID implementation on AT91SAM7x

--- src/platform/at91sam7x/platform_conf.h
+++ src/platform/at91sam7x/platform_conf.h
@@ -6,6 +6,9 @@
 #include "auxmods.h"
 #include "board.h"
 #include "stacks.h"
+#include "buf.h"
+#include "elua_int.h"
+#include "sermux.h"
 
 // *****************************************************************************
 // Define here what components you want for this platform
@@ -16,11 +19,15 @@
 #define BUILD_TERM
 #define BUILD_CON_GENERIC
 //#define BUILD_RPC
+#define BUILD_RFS
+#define BUILD_SERMUX
+#define BUILD_C_INT_HANDLERS
 
 // *****************************************************************************
 // UART/Timer IDs configuration data (used in main.c)
 
-#define CON_UART_ID           0
+#define CON_UART_ID         ( SERMUX_SERVICE_ID_FIRST + 1 )
+//#define CON_UART_ID         0
 #define CON_UART_SPEED        115200
 #define CON_TIMER_ID          0
 #define TERM_LINES            25
@@ -33,9 +40,6 @@
 #define RPCLINE _ROM( AUXLIB_RPC, luaopen_rpc, rpc_map )
 #else
 #define RPCLINE
-#if !defined( BUILD_RPC )
-#define BUILD_RPC
-#endif
 #endif
 
 #define LUA_PLATFORM_LIBS_ROM\
@@ -78,8 +82,8 @@
 #define RPC_UART_SPEED        CON_UART_SPEED
 
 // Enable RX buffering on UART
-//#define BUF_ENABLE_UART
-//#define CON_BUF_SIZE          BUF_SIZE_128
+#define BUF_ENABLE_UART
+#define CON_BUF_SIZE          BUF_SIZE_128
 
 // CPU frequency (needed by the CPU module, 0 if not used)
 #define CPU_FREQUENCY         BOARD_MCK
@@ -102,5 +106,23 @@
 #endif
 #define MEM_START_ADDRESS     { ( void* )end }
 #define MEM_END_ADDRESS       { ( void* )( SRAM_ORIGIN + SRAM_SIZE - STACK_SIZE_TOTAL - 1 ) }
+
+#define RFS_BUFFER_SIZE       BUF_SIZE_512
+#define RFS_UART_ID           ( SERMUX_SERVICE_ID_FIRST )
+#define RFS_TIMER_ID          0
+#define RFS_TIMEOUT           100000
+#define RFS_UART_SPEED        115200
+
+#define SERMUX_PHYS_ID        0
+#define SERMUX_PHYS_SPEED     115200
+#define SERMUX_NUM_VUART      2
+#define SERMUX_BUFFER_SIZES   { RFS_BUFFER_SIZE, CON_BUF_SIZE }
+
+// Interrupt list
+#define INT_UART_RX           ELUA_INT_FIRST_ID
+#define INT_ELUA_LAST         INT_UART_RX
+
+#define PLATFORM_CPU_CONSTANTS\
+ _C( INT_UART_RX )
 
 #endif // #ifndef __PLATFORM_CONF_H__
---
+++ src/platform/at91sam7x/platform_int.c
@@ -0,0 +1,117 @@
+// AVR32 interrupt support
+
+#include "platform_conf.h"
+#if defined( BUILD_C_INT_HANDLERS ) || defined( BUILD_LUA_INT_HANDLERS )
+
+// Generic headers
+#include "platform.h"
+#include "elua_int.h"
+#include "common.h"
+
+// Platform includes
+#include "aic.h"
+#include <board.h>
+
+// "Stubs" used for our interrupt handlers
+// Just a trick to avoid interworking and some other complications
+
+#define INT_STUB( func )\
+  asm volatile(\
+  "push {lr}\n\t"\
+  "bl   " #func "\n\t"\
+  "pop  {r0}\n\t"\
+  "bx   r0\n\t"\
+ )\
+ 
+// ****************************************************************************
+// Interrupt handlers
+
+// ----------------------------------------------------------------------------
+// UART_RX interrupt
+
+static const int usart_int_ids[] = { AT91C_ID_US0, AT91C_ID_US1 };
+static AT91S_USART* const usart_bases[] = { AT91C_BASE_US0, AT91C_BASE_US1 };
+ 
+static void uart_common_rx_handler( int resnum )
+{
+  cmn_int_handler( INT_UART_RX, resnum );
+  AT91C_BASE_AIC->AIC_ICCR = 1 << usart_int_ids[ resnum ];
+  AT91C_BASE_AIC->AIC_EOICR = 0;
+}
+
+void __uart0_rx_handler_helper()
+{
+  uart_common_rx_handler( 0 );
+}
+
+static void __attribute__((naked)) uart0_rx_handler()
+{
+    INT_STUB( __uart0_rx_handler_helper );
+}
+
+void __uart1_rx_handler_helper()
+{
+  uart_common_rx_handler( 1 );
+}
+
+static void __attribute__((naked)) uart1_rx_handler()
+{
+    INT_STUB( __uart1_rx_handler_helper );
+}
+
+// ****************************************************************************
+// Interrupt: INT_UART_RX
+
+static int int_uart_rx_get_status( elua_int_resnum resnum )
+{
+  volatile AT91S_USART *pusart = ( volatile AT91S_USART* )usart_bases[ resnum ];
+  return ( pusart->US_IMR & AT91C_US_RXRDY ) ? 1 : 0;
+}
+
+static int int_uart_rx_set_status( elua_int_resnum resnum, int status )
+{
+  volatile AT91S_USART *pusart = ( volatile AT91S_USART* )usart_bases[ resnum ];
+  int prev = int_uart_rx_get_status( resnum );
+  
+  if( status == PLATFORM_CPU_ENABLE )      
+    pusart->US_IER = AT91C_US_RXRDY;
+  else
+    pusart->US_IDR = AT91C_US_RXRDY;    
+  return prev;
+}
+
+static int int_uart_rx_get_flag( elua_int_resnum resnum, int clear )
+{
+  volatile AT91S_USART *pusart = ( volatile AT91S_USART* )usart_bases[ resnum ];
+
+  ( void )clear; // Note: this interrupt will be cleared automatically when the RHR is read
+  return( pusart->US_CSR & AT91C_US_RXRDY ) ? 1 : 0;    
+}
+
+// ****************************************************************************
+// Interrupt initialization
+
+typedef void ( *phandler )();
+static phandler phandlers[] = { uart0_rx_handler, uart1_rx_handler };
+
+void platform_int_init()
+{
+  unsigned i;
+  
+  for( i = 0; i < NUM_UART; i ++ )
+  {
+    AIC_ConfigureIT( usart_int_ids[ i ], 0, phandlers[ i ] ); 
+    AIC_EnableIT( usart_int_ids[ i ] );
+  }  
+}
+
+// ****************************************************************************
+// Interrupt table
+// Must have a 1-to-1 correspondence with the interrupt enum in platform_conf.h!
+
+const elua_int_descriptor elua_int_table[ INT_ELUA_LAST ] = 
+{
+  { int_uart_rx_set_status, int_uart_rx_get_status, int_uart_rx_get_flag }
+};
+
+#endif // #if defined( BUILD_C_INT_HANDLERS ) || defined( BUILD_LUA_INT_HANDLERS )
