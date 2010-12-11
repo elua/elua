a148d3956e436cf2bf4cb84cbe761939319c28be
Dado Sutter <dadosutter@gmail.com>
2010-12-11 00:24:17 +0000

    Changing eLua version for the next release (v0.8)

--- inc/version.h
+++ inc/version.h
@@ -3,7 +3,7 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-#define     ELUA_VERSION          trunk pos0.7
-#define     ELUA_STR_VERSION      "trunk pos0.7"
+#define     ELUA_VERSION          v0.8
+#define     ELUA_STR_VERSION      "v0.8"
 
 #endif
