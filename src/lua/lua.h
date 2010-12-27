b7b943919cb69fa94cc3b57e87f774398a8994da
Dado Sutter <dadosutter@gmail.com>
2010-12-27 12:03:16 +0000

    - Updating Lua copyright notice to extend to 2011

--- src/lua/lua.h
+++ src/lua/lua.h
@@ -19,7 +19,7 @@
 #define LUA_VERSION	"Lua 5.1"
 #define LUA_RELEASE	"Lua 5.1.4"
 #define LUA_VERSION_NUM	501
-#define LUA_COPYRIGHT	"Copyright (C) 1994-2008 Lua.org, PUC-Rio"
+#define LUA_COPYRIGHT	"Copyright (C) 1994-2011 Lua.org, PUC-Rio"
 #define LUA_AUTHORS 	"R. Ierusalimschy, L. H. de Figueiredo & W. Celes"
 
 
