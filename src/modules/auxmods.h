c39d55feb8d2f147972bdb84bb366724a0bb5376
Martin Guy <martinwguy@gmail.com>
2011-07-23 19:09:09 +0200

    Revert "Allow "disp" modules to compile when optram=0"
    This reverts commit c027a5929391550d25e8a6c5edc0de8e46d30708.

--- src/modules/auxmods.h
+++ src/modules/auxmods.h
@@ -57,11 +57,6 @@ LUALIB_API int ( luaopen_elua )( lua_State *L );
 #define AUXLIB_I2C  "i2c"
 LUALIB_API int ( luaopen_i2c )( lua_State *L );
 
-// Platform-specific modules
-#define AUXLIB_DISP  "disp"
-LUALIB_API int ( luaopen_disp )( lua_State *L );
-
-
 // Helper macros
 #define MOD_CHECK_ID( mod, id )\
   if( !platform_ ## mod ## _exists( id ) )\
