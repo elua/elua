c9c4311f159b2fc98a5c96947929d062384b42ed
Martin Guy <martinwguy@gmail.com>
2011-12-18 18:36:36 +0100

    Apply fix to Lua 5.1.4 bug #11
    See http://www.lua.org/bugs.html -> Lua 5.1.4 -> bug 11
    "Parser may collect a prototype while building it."

--- src/lua/lparser.c
+++ src/lua/lparser.c
@@ -375,9 +375,9 @@ static void close_func (LexState *ls) {
   lua_assert(luaG_checkcode(f));
   lua_assert(fs->bl == NULL);
   ls->fs = fs->prev;
-  L->top -= 2;  /* remove table and prototype from the stack */
   /* last token read was anchored in defunct function; must reanchor it */
   if (fs) anchor_token(ls);
+  L->top -= 2;  /* remove table and prototype from the stack */
 }
 
 
