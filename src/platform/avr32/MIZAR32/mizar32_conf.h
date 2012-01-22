21bd14dc84d4e7891d8dd33d7bc020bcd5565b8d
Nuccio Raciti <raciti.nuccio@gmail.com>
2012-01-22 21:40:57 +0100

    Changed the way you configure the target
    Starting from now, any -DBUILD_XXXX define is passed inside
    the DEFINES environment variable...
    
    Signed-off-by: Nuccio Raciti <raciti.nuccio@gmail.com>

--- src/platform/avr32/MIZAR32/mizar32_conf.h
+++ src/platform/avr32/MIZAR32/mizar32_conf.h
@@ -12,36 +12,12 @@
 #include "buf.h"
 
 // *****************************************************************************
-// Define here what components you want for this platform
-
-#define BUILD_MMCFS
-//#define BUILD_ROMFS
-#define BUILD_CON_GENERIC
-//#define BUILD_RPC
-#define BUF_ENABLE_UART
-#define BUILD_C_INT_HANDLERS
-//#define BUILD_RFS
-//#define BUILD_SERMUX
-
 #if defined( ELUA_CPU_AT32UC3A0128 )
   // Build options for 120KB image
 # define RAM_SIZE 0x8000
 #else
   // Build options for 256KB and 512KB flash
 # define RAM_SIZE 0x10000
-# define BUILD_SHELL
-# define BUILD_XMODEM
-# define BUILD_ADC
-# define BUILD_LCD
-# define BUILD_TERM
-# define BUILD_UIP
-# define BUILD_LUA_INT_HANDLERS
-#endif
-
-#ifdef BUILD_UIP
-#define BUILD_DHCPC
-#define BUILD_DNS
-//#define BUILD_CON_TCP
 #endif
 
 // ****************************************************************************
@@ -173,7 +149,7 @@
 // can copy/paste program fragments or data into the serial console.
 // An 80x25 screenful is 2000 characters so we use 2048 and the buffer is
 // allocated from the 32MB SDRAM so there is no effective limit.
-#define CON_BUF_SIZE          BUF_SIZE_2048
+// #define CON_BUF_SIZE          BUF_SIZE_2048
 
 // RPC boot options
 #define RPC_UART_ID           CON_UART_ID
