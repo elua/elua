27da4951510236164a365aa6b8c2249ca601ac3d
Martin Guy <martinwguy@gmail.com>
2012-03-08 21:23:13 +0100

    Document pio.decode()

--- doc/eluadoc/refman_gen_pio.lua
+++ doc/eluadoc/refman_gen_pio.lua
@@ -151,6 +151,19 @@ data_en =
         "$portn (optional)$ - the %n%-th port"
       }
     },
+
+    { sig = "port, pin = #pio.decode#( resnum )",
+      desc = "Convert a PIO resource number to the corresponding port and pin. This is most commonly used in GPIO edge interrupt routines to convert the Lua interrupt routine's argument to the port and pin that caused the interrupt but it can also be used on the values returned by the pin names pio.PA_0, pio.P2_15 and so on.",
+      args =
+      {
+        "$resnum$ - the resource number of the pin",
+      },
+      ret =
+      {
+        "$port$ - the index of the port, starting from 0 (so port A is 0, port B is 1 and so on)",
+        "$pin$ - the pin number, usually from 0 to 31",
+      },
+    },
    
   }
 
