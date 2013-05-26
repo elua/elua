96fcd19e7c76e09279156347424933405ad0ad49
Bogdan Marinescu <bogdan.marinescu@gmail.com>
2013-05-26 21:34:24 +0300

    Added more components to the simulator
    Also, I should really document the simulator at some point. Not after
    a week-end of writing and updating docs though.

--- boards/known/sim.lua
+++ boards/known/sim.lua
@@ -6,8 +6,9 @@ return {
     sercon = { uart = 0, speed = 0 },
     wofs = true,
     romfs = true,
-    shell = true,
+    advanced_shell = true,
     term = { lines = 25, cols = 80 },
+    mmcfs = { spi = 0, cs_port = 0, cs_pin = 0 },
   },
   modules = {
     generic = { 'pd', 'all_lua', 'term', 'elua' }
