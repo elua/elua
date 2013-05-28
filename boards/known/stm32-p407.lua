96ae53d26942e0e7555c7278bba140872306adb3
Bogdan Marinescu <bogdan.marinescu@gmail.com>
2013-05-29 00:58:35 +0300

    STM32F4 fixes
    - added STM32-P407 board from Olimex (no ext SRAM yet)
    - added documentation for the new configurator parameters
    
    Signed-off-by: Bogdan Marinescu <bogdan.marinescu@gmail.com>

---
+++ boards/known/stm32-p407.lua
@@ -0,0 +1,26 @@
+return {
+  cpu = 'stm32f407vg',
+  components = {
+    sercon = { uart = 2, speed = 115200 },
+    romfs = true,
+    advanced_shell = true,
+    term = { lines = 25, cols = 80 },
+    linenoise = { shell_lines = 10, lua_lines = 50 },
+    rpc = { uart = 2, speed = 115200 },
+    adc = { buf_size = 2 },
+    xmodem = true,
+    cints = true, 
+    luaints = true
+  },
+  config = {
+    egc = { mode = "alloc" },
+    vtmr = { num = 4, freq = 10 },
+    ram = { internal_rams = 2 },
+    clocks = { external = 25000000, cpu = 168000000 }
+  },
+  modules = {
+    generic = { 'all', "-i2c", "-net" },
+    platform = 'all',
+  },
+}
+
