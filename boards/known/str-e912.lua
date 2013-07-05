1b3a9759d4c1399d50f6b4bef742a68b6c77b2b5
mauricio <mauricio@mauricio-desktop-i7.(none)>
2013-07-05 20:07:03 -0300

    Added mmcfs configuration for STR-E912 board (note: set the card_cs_sel jumper to the position closer to the JTAG).

--- boards/known/str-e912.lua
+++ boards/known/str-e912.lua
@@ -12,7 +12,8 @@ return {
     linenoise = { shell_lines = 10, lua_lines = 50 },
     rpc = { uart = 0, speed = 115200 },
     adc = { buf_size = 2 },
-    xmodem = true
+    xmodem = true,
+    mmcfs = { spi = 0, cs_port = 2, cs_pin = 7 },
   },
   config = {
     vtmr = { num = 4, freq = 16 },
