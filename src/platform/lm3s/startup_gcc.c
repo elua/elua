307de046e22ad50ef57846ad65ad5ed39cbd2213
ecdr <Github.0hv@gishpuppy.com>
2014-03-12 18:37:34 -0800

    LM3S - startup_gcc - use console UART, rather than assuming UART0

--- src/platform/lm3s/startup_gcc.c
+++ src/platform/lm3s/startup_gcc.c
@@ -70,6 +70,8 @@ extern void tmr3_handler();
 extern void USB0DeviceIntHandler(void);
 #endif
 
+// From platform.c
+extern const u32 uart_base[];
 
 //*****************************************************************************
 //
@@ -261,9 +263,7 @@ ResetISR(void)
 #include "sysctl.h"
 #include "uart.h"
 
-// FIXME: Use console UART for error indicators
-// Instead of UART0_BASE, should use uart_base[CON_UART_ID]
-// Still would not be right for USB_CDC
+// FIXME: Assumes console is UART, will not be right for USB_CDC
 
 //*****************************************************************************
 //
@@ -275,15 +275,15 @@ ResetISR(void)
 static void
 NmiSR(void)
 {
-    MAP_UARTCharPut( UART0_BASE, 'N' );
-    MAP_UARTCharPut( UART0_BASE, 'M' );
-    MAP_UARTCharPut( UART0_BASE, 'I' );
+    MAP_UARTCharPut( uart_base[CON_UART_ID], 'N' );
+    MAP_UARTCharPut( uart_base[CON_UART_ID], 'M' );
+    MAP_UARTCharPut( uart_base[CON_UART_ID], 'I' );
     //
     // Enter an infinite loop.
     //
     while(1)
     {
-      MAP_UARTCharPut( UART0_BASE, '!' );
+      MAP_UARTCharPut( uart_base[CON_UART_ID], '!' );
     }
 }
 
@@ -300,11 +300,11 @@ FaultISR(void)
     //
     // Enter an infinite loop.
     //
-   MAP_UARTCharPut( UART0_BASE, 'F' );
-   MAP_UARTCharPut( UART0_BASE, 'a' );
-   MAP_UARTCharPut( UART0_BASE, 'u' );
-   MAP_UARTCharPut( UART0_BASE, 'l' );
-   MAP_UARTCharPut( UART0_BASE, 't' );
+   MAP_UARTCharPut( uart_base[CON_UART_ID], 'F' );
+   MAP_UARTCharPut( uart_base[CON_UART_ID], 'a' );
+   MAP_UARTCharPut( uart_base[CON_UART_ID], 'u' );
+   MAP_UARTCharPut( uart_base[CON_UART_ID], 'l' );
+   MAP_UARTCharPut( uart_base[CON_UART_ID], 't' );
    while(1)
    {
    }
@@ -320,14 +320,14 @@ FaultISR(void)
 static void
 IntDefaultHandler(void)
 {
-  MAP_UARTCharPut( UART0_BASE, 'I' );
-  MAP_UARTCharPut( UART0_BASE, 'n' );
-  MAP_UARTCharPut( UART0_BASE, 't' );
+  MAP_UARTCharPut( uart_base[CON_UART_ID], 'I' );
+  MAP_UARTCharPut( uart_base[CON_UART_ID], 'n' );
+  MAP_UARTCharPut( uart_base[CON_UART_ID], 't' );
     //
     // Go into an infinite loop.
     //
     while(1)
     {
-      MAP_UARTCharPut( UART0_BASE, '*' );
+      MAP_UARTCharPut( uart_base[CON_UART_ID], '*' );
     }
 }
