53aed812c7ba754e8f21833165a5c99489898157
Raman <ramangopalan@gmail.com>
2016-08-02 15:01:55 +0530

    Update Infineon XMC4500 LLDs + add support for SD card interface

--- src/platform/xmc4000/conf.lua
+++ src/platform/xmc4000/conf.lua
@@ -1,10 +1,19 @@
+
 -- Configuration file for the XMC4500 microcontroller
 
-specific_files = "platform.c startup_XMC4500.s lib/system_XMC4500.c lib/UART001.c lib/UART001_Conf.c lib/RESET001.c lib/CLK001.c lib/DAVE3.c lib/MULTIPLEXER.c"
+specific_files = "platform.c startup_XMC4500.S system_XMC4500.c xmclib/src/xmc4_eru.c xmclib/src/xmc4_flash.c xmclib/src/xmc4_gpio.c xmclib/src/xmc4_rtc.c xmclib/src/xmc4_scu.c xmclib/src/xmc_can.c xmclib/src/xmc_ccu4.c xmclib/src/xmc_ccu8.c xmclib/src/xmc_common.c xmclib/src/xmc_dac.c xmclib/src/xmc_dma.c xmclib/src/xmc_dsd.c xmclib/src/xmc_ebu.c xmclib/src/xmc_ecat.c xmclib/src/xmc_eru.c xmclib/src/xmc_eth_mac.c xmclib/src/xmc_fce.c xmclib/src/xmc_gpio.c xmclib/src/xmc_hrpwm.c xmclib/src/xmc_i2c.c xmclib/src/xmc_i2s.c xmclib/src/xmc_ledts.c xmclib/src/xmc_posif.c xmclib/src/xmc_rtc.c xmclib/src/xmc_sdmmc.c xmclib/src/xmc_spi.c xmclib/src/xmc_uart.c xmclib/src/xmc_usbd.c xmclib/src/xmc_usic.c xmclib/src/xmc_vadc.c xmclib/src/xmc_wdt.c xmclib/gen/CLOCK_XMC4/clock_xmc4.c xmclib/gen/CLOCK_XMC4/clock_xmc4_conf.c xmclib/gen/UART/uart.c xmclib/gen/UART/uart_conf.c xmclib/gen/CPU_CTRL_XMC4/cpu_ctrl_xmc4.c xmclib/gen/CPU_CTRL_XMC4/cpu_ctrl_xmc4_conf.c xmclib/gen/FATFS/fatfs.c xmclib/gen/FATFS/fatfs_conf.c xmclib/gen/FATFS/sltha.c xmclib/gen/RTC/rtc.c xmclib/gen/RTC/rtc_conf.c xmclib/gen/SDMMC_BLOCK/sdmmc_block_private_sd.c xmclib/gen/SDMMC_BLOCK/sdmmc_block.c xmclib/gen/SDMMC_BLOCK/sdmmc_block_conf.c xmclib/gen/SYSTIMER/systimer.c xmclib/gen/SYSTIMER/systimer_conf.c xmclib/gen/DAVE.c"
 
-local ldscript = "xmc4500f144k1024.ld"
+local ldscript = "linker_script.ld"
 
-addi( "src/platform/" .. platform .. "/lib" )
+addi( "src/platform/" .. platform .. "/xmclib/inc" )
+addi( "src/platform/" .. platform .. "/xmclib/gen" )
+addi( "src/platform/" .. platform .. "/xmclib/gen/UART" )
+addi( "src/platform/" .. platform .. "/xmclib/gen/CLOCK_XMC4" )
+addi( "src/platform/" .. platform .. "/xmclib/gen/CPU_CTRL_XMC4" )
+addi( "src/platform/" .. platform .. "/xmclib/gen/FATFS" )
+addi( "src/platform/" .. platform .. "/xmclib/gen/RTC" )
+addi( "src/platform/" .. platform .. "/xmclib/gen/SDMMC_BLOCK" )
+addi( "src/platform/" .. platform .. "/xmclib/gen/SYSTIMER" )
   
 -- Prepend with path
 specific_files = utils.prepend_path( specific_files, "src/platform/" .. platform )
@@ -12,7 +21,8 @@ specific_files = specific_files .. " src/platform/cortex_utils.s src/platform/ar
 ldscript = sf( "src/platform/%s/%s", platform, ldscript )
 
 addm( { "FOR" .. cnorm( comp.cpu ), "FOR" .. cnorm( comp.board ), 'gcc', 'CORTEX_M4' } )
-addm( { "UC_ID=4502", "DAVE_CE" } )
+--addm( { "UC_ID=4502", "DAVE_CE" } )
+addm( { "XMC4500_F144x1024" } )
 
 -- Standard GCC Flags
 addcf( { '-ffunction-sections', '-fdata-sections', '-fno-strict-aliasing', '-Wall' } )
@@ -24,7 +34,7 @@ local target_flags = { '-mcpu=cortex-m4', '-mthumb', '-mfloat-abi=soft' }
 
 -- Configure general flags for target
 addcf( { target_flags, '-mlittle-endian' } )
-addlf( { target_flags, '-Wl,-e,__Xmc4500_reset_cortex_m', '-Wl,-static' } )
+addlf( { target_flags, '-Wl,-e,Reset_Handler', '-Wl,-static' } )
 addaf( target_flags )
 
 -- Toolset data
---
+++ src/platform/xmc4000/xmclib/gen/DAVE_common.h
@@ -0,0 +1,61 @@
+
+/**************************************************************************//**
+ *
+ * Copyright (C) 2014 Infineon Technologies AG. All rights reserved.
+ *
+ * Infineon Technologies AG (Infineon) is supplying this software for use with
+ * Infineon's microcontrollers.
+ * This file can be freely distributed within development tools that are
+ * supporting such microcontrollers.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ * OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ * INFINEON SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ * OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ *
+********************************************************************************
+**                                                                            **
+**                                                                            **
+** PLATFORM : Infineon <XMC4000/1000 series>                                  **
+**                                                                            **
+** COMPILER : Compiler Independent                                            **
+**                                                                            **
+** MODIFICATION DATE : Dec 27, 2014                                           **
+**                                                                            **
+*******************************************************************************/
+
+/**
+ * @file  DAVE_common.h
+ *
+ * @brief This file contains all app related public data structures,enums and function
+ *        prototypes
+ *
+ * Revision History
+ * 27 Dec 2014 v0.1.0 Initial Version
+ *
+ */
+
+#ifndef _DAVE_COMMON_H_
+#define _DAVE_COMMON_H_
+
+/*******************************************************************************
+**                      @Project Includes                                     **
+*******************************************************************************/
+
+/*******************************************************************************
+**                      Global data structures                                **
+*******************************************************************************/
+typedef struct DAVE_APP_VERSION
+{
+  uint8_t major;
+  uint8_t minor;
+  uint8_t patch;
+} DAVE_APP_VERSION_t;
+
+/*******************************************************************************
+**                      @Prototypes Of Global Functions                       **
+*******************************************************************************/
+
+#endif  /** ifndef _DAVE_COMMON_H_ */
+
