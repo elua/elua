02b8e4c9bd4d665cefaee8e464b91a410a1edc4f
Raman <ramangopalan@gmail.com>
2017-02-04 19:58:12 +0530

    XMC4500 proto-card-v2: Add initial eLua support

--- src/platform/xmc4000/build_config.lua
+++ src/platform/xmc4000/build_config.lua
@@ -18,7 +18,9 @@ local at = require "attributes"
 -- Add specific components to the 'components' table
 function add_platform_components( t, board, cpu )
   board = board:upper()
-  if board == 'XMC4500-HEXAGON' or board == 'XMC4500-HEXAGON-SDRAM' then
+  if board == 'XMC4500-HEXAGON' or
+     board == 'XMC4500-HEXAGON-SDRAM' or
+     board == 'XMC4500-PROTOCARD-V2' then
     t.xmc45_pot = { macro = 'ENABLE_POTENTIOMETER' }
     t.xmc45_dts = { macro = 'ENABLE_DTS' }
     t.xmc45_rtc = { macro = 'ENABLE_RTC' }
