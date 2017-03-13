af6b525121a6ac51db88be8f2315dc08607a2f0d
Raman <ramangopalan@gmail.com>
2017-03-13 18:28:07 +0530

    New stm32 target: elua-zeisig-gemacht

---
+++ boards/known/elua-zeisig-gemacht.lua
@@ -0,0 +1,45 @@
+
+--[[
+
+Zeisig Labortechnik: eLua board file for the cryogenic machine's
+processor board
+
+Notes:
+
+1) This machine controls the internal temperature of specifically
+designed viscometry baths with a stability of +/- 0.01 degree C. As
+much as I love to see the measurements of a viscometer (usually, the
+computation of Billmeyer viscosity of ploymers dissolved in Formic
+acid), I personally also love to see eLua on every target platform I
+work with. What is fun without SpaceShip OR Game of life?
+
+R
+
+--]]
+
+return {
+  cpu = 'stm32f103vct6',
+  components = {
+    mmcfs = { spi = 0, cs_port = 4, cs_pin = 0 },
+    sercon = { uart = 0, speed = 115200, buf_size = 128 },
+    wofs = false,
+    romfs = true,
+    shell = true,
+    term = { lines = 25, cols = 80 },
+    cints = true,
+    luaints = true,
+    linenoise = { shell_lines = 10, lua_lines = 50 },
+    stm32_enc = true,
+    rpc = { uart = 0, speed = 115200 },
+    adc = { buf_size = 4 },
+    xmodem = true
+  },
+  config = {
+    egc = { mode = "alloc" },
+    vtmr = { num = 4, freq = 10 },
+  },
+  modules = {
+    generic = { 'all', '-i2c', '-net', '-can' },
+    platform = 'all',
+  }
+}
