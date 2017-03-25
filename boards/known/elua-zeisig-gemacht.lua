61a9c626c79f82ba52309df216fb7232f2800901
Raman <ramangopalan@gmail.com>
2017-03-25 18:10:59 +0530

    SDMMC: Correct CS pin to resolve access to SD card

--- boards/known/elua-zeisig-gemacht.lua
+++ boards/known/elua-zeisig-gemacht.lua
@@ -20,7 +20,7 @@ R
 return {
   cpu = 'stm32f103vct6',
   components = {
-    mmcfs = { spi = 0, cs_port = 4, cs_pin = 0 },
+    mmcfs = { spi = 0, cs_port = 0, cs_pin = 4 },
     sercon = { uart = 0, speed = 115200, buf_size = 128 },
     wofs = false,
     romfs = false,
