dc8f116e635d588427ad40112f5a38408e33070d
Raman <ramangopalan@gmail.com>
2019-02-02 19:09:06 +0530

    XMC4500 SDRAM kit: Update notes section in board configuration

--- boards/known/xmc4500-hexagon-sdram.lua
+++ boards/known/xmc4500-hexagon-sdram.lua
@@ -12,6 +12,32 @@ Notes:
    talk to the ISSI SDRAM controller. So, on this board, it is all
    about SDRAM. To have an SD card interface, SPI is the only way.
 
+3) To hook up an external SD card adapter, use the following pin
+   configurations:
+
+   MISO pin: #D6 (P3.14) / FLASH_QSPI_IO1
+   MOSI pin: #D7 (P3.15) / FLASH_QSPI_IO0
+   SCLKOUT pin: #C5 (P0.13) / FLASH_QSPI_CLK
+   Slave select 0 pin: #E7 (P3.4)
+
+   I used DAVE4 to generate the boilerplate configuration with DMA
+   transfers enabled for SPI.
+
+4) Refer to the XMC4500E144K1024 board reference manual. The external
+   SD card adapter should be connected to the HMI interface using the
+   KitSat connector.
+
+5) I ran Dimitry Grinberg's uARM on this controller after many years
+   using configuration 3). If you choose to do this, there's not a lot
+   you can do thereafter. Forget RTC. And please certainly forget the
+   custom LCD shield.
+
+6) Clearly, 32GB Samsung cards have a problem with higher SPI bus
+   speeds. It works fine at 5000 kHz to 8000 KHz. It however fails
+   with the (generated) default of 10000 KHz.
+
+Happy hacking!
+
 --]]
 
 
